e2cdce59915d1f1babc110ebe7da21236cda87ff
a0909007892 <a0909007892@gmail.com>
2023-02-08 13:34:46 +0800

    collection

---
+++ study/src/test/java/stream/CollectionImpl.java
@@ -0,0 +1,19 @@
+package stream;
+
+public class CollectionImpl {
+
+  public static void main(String[] args) {
+
+
+  }
+
+  // static<T, R> void demo1(T... values){
+  //   R container = Collectors
+  // }
+
+  // static SortedSet<String> demo2(String... str){
+
+  // }
+
+
+}
